9b25716f6e26ec8b14c3b4805ebc874ef5dc1ec0
Sylvain Julé <sylvain.jule@free.fr>
2017-07-13 14:17:34 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
-# kirby-illustrated-checkboxes
-Add images to Kirby's checkboxes
+# Imageboxes - Kirby Illustrated Checkboxes
+
+This field allows you to add illustrations to checkboxes. Suggestions welcome.
+
+![illustrated-checkboxes](https://user-images.githubusercontent.com/14079751/28165472-2ed0c9e4-67d4-11e7-9cde-535561ad4725.jpg)
+
+## Installation
+Put this field in the `site/fields` directory.  
+The field folder must be named `imageboxes` :
+
+```
+|-- site/fields/
+    |-- imageboxes/
+        |-- assets/
+        |-- imageboxes.php
+```
+
+## Usage
+
+Basic usage in blueprint:
+```yaml
+  fieldname:
+    label: Field label
+    type: imageboxes
+    columns: 4
+    options: 
+      first:
+        label: Char. 01
+        image: first.jpg
+      second:
+        label: Char. 02
+        image: second.jpg
+      third:
+        label: Char. 03
+        image: third.jpg
+      fourth:
+        label: Char. 04
+        image: fourth.jpg
+```
+
+By default, images must be put in the main `assets/images` folder of your website.
+
+## Options
+
+Other options are not required. 
+
+### Custom ratio
+
+If `ratio` is specified, images will be displayed as background images and the ratio set for its container. You can then set the background position with a CSS syntax (not mandatory, default position is : `center center`).
+```yaml
+  fieldname:
+    label: Field label
+    type: imageboxes
+    columns: 4
+    display:
+      ratio: 3/2
+      position: top left
+    options:
+      (...)
+```
+
+### Enable for mobiles
+
+By default, images are not displayed when the panel switches to its mobile view. If you want to override this, set :
+```yaml
+    display:
+      mobile: true
+```
+
+### Fetch images
+
+You can query images from existing pages to populate the buttons.
+
+Please note that `fetch` **must** be set to `images` in order for this to work properly.
+
+The appropriate syntax is then :
+
+```yaml
+  fieldname:
+    label: Field label
+    type: imageboxes
+    columns: 3
+    options: query
+    query:
+      page: path/to/page
+      fetch: images
+      value: '{{filename}}'
+      text: 
+        label: '{{filename}}'
+        image: '{{filename}}'
+```
+
+## License
+
+MIT
---
+++ fields/imageboxes/imageboxes.php
@@ -0,0 +1,254 @@
+<?php
+
+class ImageboxesField extends InputField {
+
+    static public $assets = array(
+        'css' => array(
+            'styles.css'
+        )
+    );
+    
+    public $columns = 2;
+    protected $cache;
+
+    public function value() {
+        $value = parent::value();/*
+        if(empty($value)) {
+            // get the first key of options
+            $options = $this->options();
+            if(is_array($options)) {
+                reset($options);
+                $value = key($options);        
+            }
+        }*/
+        if(!is_array($value)) {
+            $value = str::split($value, ',');
+        }
+
+        // Remove items from value array that are not present in the options array
+        return array_keys(array_intersect_key(array_flip($value), $this->options()));
+    }
+    public function input() {
+        $value   = func_get_arg(0);
+        $input = parent::input($value);
+        $input->removeClass('input');
+        $input->addClass('checkbox');
+        $input->attr('type', 'radio');
+        $input->attr(array(
+            'name'     => $this->name() . '[]',
+            'type'     => 'checkbox',
+            'value'    => $value,
+            'checked'  => ($this->value === 'all') ? true : in_array($value, (array)$this->value()),
+            'required' => false,
+        ));
+        
+        if($this->readonly) {
+            $input->attr('disabled', true);      
+        }
+        
+        return $input;
+    }
+    public function label() {
+        $label = parent::label();
+        if(is_null($label)) return null;
+
+        // use a legend to avoid having a label
+        // that is just connected to the first input
+        return $label->tag('legend')->attr('for', false);
+    }
+    public function options() {
+        if($this->cache) return $this->cache;
+
+        return $this->cache = fieldoptions::build($this);
+    }
+
+
+    public function item($value, $options) {
+
+        $input = $this->input($value);
+        
+        // Get label and filename of the input
+        $text = $options['label'];
+        $image = $options['image'];
+        $display = $this->display();
+        
+        
+        /* Get image URL
+        ----------------------------*/
+        
+        // If a query is made about images, set parent directory as page URI or 'page' option 
+        if ($this->query() && $this->query()['fetch'] == 'images') {
+            
+            $query = $this->query();
+            
+            // If there is a 'page' options -> Get the page URI
+            if (array_key_exists('page', $query)) {
+                $uri = $this->getPage($query['page'])->uri();
+            } 
+            // If there is no 'page' option -> Get the current page URI
+            else {
+                $uri = $this->page()->uri();
+            }
+            
+            $imageurl = kirby()->urls->index() . '/' . $uri . '/' . $image;
+        }
+        // Otherwise, image is to be found in the main assets/images folder
+        else {
+            $imageurl = kirby()->urls()->assets() .'/images/'. $image;
+        }
+        
+        
+        /* Mobile option
+        ----------------------------*/
+        
+        // default : false
+        $mobile = false;
+        
+        // Get mobile option if specified
+        if ($display && array_key_exists('mobile', $display)) {
+            $mobile = $display['mobile'];
+        }
+        
+        $mobileClass = $mobile ? '' : ' mobile-disabled';
+        
+        
+        /* If there is no ratio
+        ----------------------------*/
+        
+        // Go with an img tag if there's no ratio specified
+        if (!$display || !array_key_exists('ratio', $display)) {
+            $imageDiv = '<img src="'. $imageurl .'">';
+            $imageDiv = '<div class="checkbox-illustration'. $mobileClass .'">'. $imageDiv .'</div>';
+        }
+        
+        
+        /* If there is a ratio
+        ----------------------------*/
+        
+        if ($display && array_key_exists('ratio', $display)) {
+            
+            // Get and convert ratio (3/2 -> 66.6%)
+            $ratio = $display['ratio'];
+            $convertedRatio = 1;
+            
+            if (preg_match('/(\d+)(?:\s*)([\/])(?:\s*)(\d+)/', $ratio, $matches) !== false){
+                $convertedRatio = $matches[3] / $matches[1];
+            }
+            
+            $convertedRatio = $convertedRatio * 100;
+            $convertedRatio = round($convertedRatio, 3, PHP_ROUND_HALF_DOWN);
+            $ratio = $convertedRatio;
+            
+            // Get position if specified | default : centered
+            $position = array_key_exists('position', $display) ? $display['position'] : 'center center';
+            
+            $imageDiv = '<div class="checkbox-illustration as-background'. $mobileClass .'" style="background-image: url('. $imageurl .'); background-position: '. $position .'; padding-top: '. $ratio .'%;"></div>';
+            
+        }
+        
+        
+        /* Build the input
+        ----------------------------*/
+        
+        $label = new Brick('label');
+        $label->addClass('input');
+        $label->addClass('input-with-checkbox');
+        $label->attr('data-focus', 'true');
+        $label->append($imageDiv);
+        $label->append($input);
+        $label->append('<span>' . $this->i18n($text) . '</span>');
+
+        if($this->readonly) {
+            $label->addClass('input-is-readonly');
+        }
+
+        return $label;
+
+    }
+    
+    public function getPage($uri) {
+        
+        if(str::startsWith($uri, '../')) {
+            
+            if($currentPage = $this->page) {
+                $path = $uri;
+                while(str::startsWith($path, '../')) {
+                    if($parent = $currentPage->parent()) {
+                        $currentPage = $parent;
+                    } else {
+                        $currentPage = site();
+                    }
+                    $path = str::substr($path, 3);
+                }
+                if(!empty($path)) {
+                    $currentPage = $currentPage->find($path);
+                }
+                $page = $currentPage;
+            } else {
+                $page = null;
+            }
+        } else if($uri == '/') {
+            $page = site();
+        } else {
+            $page = page($uri);
+        }
+        return $page;
+    }
+    
+    public function content() {
+
+        $html = '<ul class="input-list field-grid cf">';
+
+        switch($this->columns()) {
+            case 2:
+                $width = ' field-grid-item-1-2';
+                break;
+            case 3:
+                $width = ' field-grid-item-1-3';
+                break;
+            case 4:
+                $width = ' field-grid-item-1-4';
+                break;
+            case 5:
+                $width = ' field-grid-item-1-5';
+                break;
+            default:
+                $width = '';
+                break;
+        }
+
+        foreach($this->options() as $key => $value) {
+            
+            
+            $html .= '<li class="input-list-item field-grid-item' . $width . '">';
+            $html .= $this->item($key, $value);
+            $html .= '</li>';
+        }
+
+        $html .= '</ul>';
+
+        $content = new Brick('div');
+        $content->addClass('field-content');
+        $content->append($html);
+
+        return $content;
+
+    }
+
+    public function result() {
+        $result = parent::result();
+        return is_array($result) ? implode(', ', $result) : '';
+    }
+    
+    public function validate() {
+        if(is_array($this->value())) {
+            foreach($this->value() as $v) {
+                if(!array_key_exists($v, $this->options())) return false;
+            }
+            return true;
+        } else {
+            return array_key_exists($this->value(), $this->options());
+        }
+    }
+
+}
