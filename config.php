78f66cb6787218f224d26b995feacd8be2f5d25c
Sylvain Julé <sylvain.jule@free.fr>
2018-11-12 09:27:51 +0100

    Clean config.php

--- config.php
+++ config.php
@@ -4,13 +4,4 @@ Kirby::plugin('sylvainjule/imageboxes', [
 	'fields' => array(
 		'imageboxes' => require_once __DIR__ . '/lib/fields/imageboxes.php',
 	),
-    'methods' => array(
-        'getImageBoxesOptions' => function () {
-            return Options::factory(
-                $this->options(),
-                $this->props,
-                $this->model()
-            );
-        },
-    )
 ]);
